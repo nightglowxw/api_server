e7dac66677404e6363cda8ceb66a001bca698561
clozz-lin <885204655@qq.com>
2022-11-17 20:11:24 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+// cors中间件
+const cors = require("cors");
+app.use(cors());
+
+// 导入并使用用户路由模块
+const userRouter = require("./router/user");
+app.use("/api", userRouter);
+
+// 解析表单数据的中间件
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(3007, () => {
+  console.log("api server running at http://127.0.0.1:3007");
+});
