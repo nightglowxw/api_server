fbdd3b7cb6c2b6ec864e36496e32ab650132c6b8
夕小汐 <885204655@qq.com>
2022-11-18 21:27:41 +0800

    update

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
+
+// 解析表单数据的中间件
+app.use(express.urlencoded({ extended: false }));
+
+
 // 导入并使用用户路由模块
 const userRouter = require("./router/user");
 app.use("/api", userRouter);
 
-// 解析表单数据的中间件
-app.use(express.urlencoded({ extended: false }));
 
 app.listen(3007, () => {
   console.log("api server running at http://127.0.0.1:3007");
--- router_handler/user.js
+++ router_handler/user.js
@@ -1,6 +1,45 @@
+// 导入数据库操作模块
+const db = require("../db/index");
+// 导入密码加密模块
+const bcrypt = require("bcryptjs");
+
 // 注册新用户
 exports.regUser = (req, res) => {
-  res.send("reguser ok");
+  // 获取客户端提交到服务器的用户信息
+  const userinfo = req.body;
+  console.log(userinfo);
+  //   对表单中的数据进行合法性校验
+  if (!userinfo.username || !userinfo.password) {
+    return res.send({
+      status: 1,
+      message: "用户名或密码不合法！",
+    });
+  }
+
+  //   定义sql语句，查询用户名是否被占用
+  const sqlStr = "select * from ev_users where username=?";
+  db.query(sqlStr, userinfo.username, (err, results) => {
+    // 执行sql语句失败
+    if (err) {
+      return res.send({
+        status: 1,
+        message: err.message,
+        mes: '执行sql语句失败'
+      });
+    }
+    // 判断用户名是否被占用
+    if (results.length > 0) {
+      return res.send({
+        status: 1,
+        message: "用户名被占用",
+      });
+    }
+    // 用户名可以使用,对密码进行加密
+    console.log(userinfo);
+    userinfo.password = bcrypt.hashSync(userinfo.password, 10);
+    console.log(userinfo);
+
+  });
 };
 
 // 登陆
